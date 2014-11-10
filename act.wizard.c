73367b4df0de263495e342550d31ae042e3129b9
Brian <Brian@BrianAlienWare>
2014-11-10 08:07:08 -0600

    Admin flags

--- act.wizard.c
+++ act.wizard.c
@@ -3221,10 +3221,10 @@ static int perform_set(struct char_data *ch, struct char_data *vict, int mode, c
 //                send_to_char(ch, "Permission denied.\r\n");
 //                return (0);
 //            }
-            if (value < ADMLVL_NONE || value > GET_ADMLEVEL(ch)) {
-                send_to_char(ch, "You can't set it to that.\r\n");
-                return (0);
-            }
+//            if (value < ADMLVL_NONE || value > GET_ADMLEVEL(ch)) {
+//                send_to_char(ch, "You can't set it to that.\r\n");
+//                return (0);
+//            }
             if (GET_ADMLEVEL(vict) == value)
                 return (1);
             admin_set(vict, value);
