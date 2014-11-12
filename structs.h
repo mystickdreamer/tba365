28f0f5c5b010ef00423271420c0d7f779dd5d3c2
Brian <Brian@BrianAlienWare>
2014-11-12 09:23:03 -0600

    Adding Races

--- structs.h
+++ structs.h
@@ -343,7 +343,7 @@
 #define CON_IBTEDIT      30 /**< OLC mode - idea/bug/typo edit */
 #define CON_MSGEDIT      31 /**< OLC mode - message editor */
 #define CON_GET_PROTOCOL 32 /**< Used at log-in while attempting to get protocols > */
-#define CON_QRACE        32 /* Choose character race*/
+#define CON_QRACE        33 /* Choose character race*/
 
 /* OLC States range - used by IS_IN_OLC and IS_PLAYING */
 #define FIRST_OLC_STATE CON_OEDIT     /**< The first CON_ state that is an OLC */
