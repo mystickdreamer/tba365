61e14fcb8a7a3ebc9522fba117b97682b459f714
Brian <Brian@BrianAlienWare>
2014-11-11 20:07:01 -0600

    Admin flags

--- structs.h
+++ structs.h
@@ -538,10 +538,10 @@
  * LVL_IMPL should always be the HIGHEST possible immortal level, and
  * LVL_IMMORT should always be the LOWEST immortal level.  The number of
  * mortal levels will always be LVL_IMMORT - 1. */
-#define LVL_IMPL    34  /**< Level of Implementors */
-#define LVL_GRGOD   33  /**< Level of Greater Gods */
-#define LVL_GOD     32  /**< Level of Gods */
-#define LVL_IMMORT	31  /**< Level of Immortals */
+//#define LVL_IMPL    34  /**< Level of Implementors */
+//#define LVL_GRGOD   33  /**< Level of Greater Gods */
+//#define LVL_GOD     32  /**< Level of Gods */
+//#define LVL_IMMORT	31  /**< Level of Immortals */
 
 /*
  * ADMLVL_IMPL should always be the HIGHEST possible admin level, and
