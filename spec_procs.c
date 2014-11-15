38ea57edab5cab792f435cf0c23b83fcfc61fcc7
Brian <Brian@BrianAlienWare>
2014-11-15 16:29:34 -0600

    trying to make skills

--- spec_procs.c
+++ spec_procs.c
@@ -30,7 +30,7 @@
 
 /* locally defined functions of local (file) scope */
 static int compare_spells(const void *x, const void *y);
-static const char *how_good(int percent);
+//static const char *how_good(int percent);
 static void npc_steal(struct char_data *ch, struct char_data *victim);
 
 /* Special procedures for mobiles. */
@@ -56,7 +56,7 @@ void sort_spells(void)
 
   qsort(&spell_sort_info[1], MAX_SKILLS, sizeof(int), compare_spells);
 }
-
+/*
 static const char *how_good(int percent)
 {
   if (percent < 0)
@@ -80,7 +80,7 @@ static const char *how_good(int percent)
 
   return " (superb)";
 }
-
+*/
 const char *prac_types[] = {
   "spell",
   "skill"
