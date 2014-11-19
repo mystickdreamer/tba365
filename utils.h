f53e7e9cffcb756616458ed2cca663b53a92fded
Brian <Brian@BrianAlienWare>
2014-11-19 03:34:25 -0600

    trying to make skills

--- utils.h
+++ utils.h
@@ -579,7 +579,8 @@ do                                                              \
 /** The room to load player ch into. */
 #define GET_LOADROOM(ch)	CHECK_PLAYER_SPECIAL((ch), ((ch)->player_specials->saved.load_room))
 /** Number of skill practice sessions remaining for ch. */
-#define GET_PRACTICES(ch)	CHECK_PLAYER_SPECIAL((ch), ((ch)->player_specials->saved.spells_to_learn))
+//#define GET_PRACTICES(ch)	CHECK_PLAYER_SPECIAL((ch), ((ch)->player_specials->saved.spells_to_learn))
+#define GET_PRACTICES(ch,cl)	CHECK_PLAYER_SPECIAL((ch), ((ch)->player_specials->class_skill_points[cl]))
 /** Current invisibility level of ch. */
 #define GET_INVIS_LEV(ch)	CHECK_PLAYER_SPECIAL((ch), ((ch)->player_specials->saved.invis_level))
 /** Current wimpy level of ch. */
