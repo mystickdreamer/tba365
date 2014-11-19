d5bc15c77c323670bd1738182e586edb8e10ab6d
Brian <Brian@BrianAlienWare>
2014-11-19 01:20:36 -0600

    trying to make skills

--- utils.h
+++ utils.h
@@ -392,9 +392,7 @@ do                                                              \
 #define GET_ARTISAN_TYPE(ch)	(ch->player.artisan_type)
 #define GET_ARTISAN_LEVEL(ch)	(GET_CLASS_RANKS(ch, CLASS_ARTISAN))
 
-#define GET_CLASS_RANKS(ch, whichclass) (IS_NPC(ch) ? ((GET_CLASS(ch) == whichclass) ? GET_LEVEL(ch) : 0 ) : \
-                                         GET_CLASS_NONEPIC(ch, whichclass) + \
-                                         GET_CLASS_EPIC(ch, whichclass))
+#define GET_CLASS_RANKS(ch, whichclass) (IS_NPC(ch) ? ((GET_CLASS(ch) == whichclass) ? GET_LEVEL(ch) : 0 ) : (GET_CLASS(ch) == whichclass))
 
 
 /** References the routine element for a spell. Currently unused. */
