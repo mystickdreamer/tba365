50429e1b334c352ae1a50eea6ae120692add12d4
Brian <Brian@BrianAlienWare>
2014-11-19 01:03:33 -0600

    trying to make skills

--- utils.h
+++ utils.h
@@ -71,6 +71,7 @@ int get_class_by_name(char *classname);
 char * convert_from_tabs(char * string);
 int count_non_protocol_chars(char * str);
 int get_skill_value(struct char_data *ch, int skillnum);
+double get_artisan_exp(struct char_data *ch);
 
 /* Public functions made available form weather.c */
 void weather_and_time(int mode);
