07d18f301eb38cb475bf887eb64d998da91802b1
Brian <Brian@BrianAlienWare>
2014-11-22 21:43:52 -0600

    spell_parser.c SKILL

--- spell_parser.c
+++ spell_parser.c
@@ -1028,6 +1028,6 @@ void mag_assign_spells(void) {
     skillo(SKILL_TRACK, "track");
     skillo(SKILL_WHIRLWIND, "whirlwind");
     skillo(SK_PERCEPTION, "perception");
-    set_skill_type(SKILL_PERCEPTION, SKTYPE_SKILL);
+    set_skill_type(SK_PERCEPTION, SKTYPE_SKILL);
 }
 
