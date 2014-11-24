68950889a9661b9f3f433b4b8ed57495578d087a
Brian <Brian@BrianAlienWare>
2014-11-23 23:49:48 -0600

    test

--- utils.h
+++ utils.h
@@ -652,9 +652,7 @@ do                                                              \
 #define SET_SKILL(ch, skill, i, val)           (ch)->player.skills[skill][i] = val
 #define GET_SKILL(ch, skill)            (ch)->player.skills[skill]
 //#define GET_SKILL_RANKS(ch, i)		((ch)->player_specials->skills[i])
-//#define GET_SKILL(ch, i)		(IS_NPC(ch) ? (spell_info[i].can_learn_skill[GET_CLASS(ch)] == SKLEARN_CLASS ? \
-					GET_LEVEL(ch) : 0) : ((ch)->skills[i]))
-//#define SET_SKILL(ch, i, val)		do { (ch)->skills[i] = val; } while(0)
+
 
 /** The player's default sector type when buildwalking */
 #define GET_BUILDWALK_SECTOR(ch) CHECK_PLAYER_SPECIAL((ch), ((ch)->player_specials->buildwalk_sector))
