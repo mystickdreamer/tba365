dcf8d66578e0b2f3c9b9af5ba4bfb4f6fd677fcd
Brian <Brian@BrianAlienWare>
2014-11-25 12:37:21 -0600

    revert test

--- modify.c
+++ modify.c
@@ -363,7 +363,7 @@ ACMD(do_skillset)
   }
   strcpy(help, (argument + 1));	/* strcpy: OK (MAX_INPUT_LENGTH <= MAX_STRING_LENGTH) */
   help[qend - 1] = '\0';
-  if ((skill = find_skill_num(help, (SKTYPE_WEAPON || SKTYPE_ARMOR || SKTYPE_SURVIVAL || SKTYPE_LORE || SKTYPE_MAGIC || SKTYPE_CRAFTING))) <= 0) {
+  if ((skill = find_skill_num(help, SKTYPE_WEAPON)) <= 0) {
     send_to_char(ch, "Unrecognized skill.\r\n");
     return;
   }
