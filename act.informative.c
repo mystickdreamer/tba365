1440e70586117265d74188d0c93dc7981e124917
Brian <Brian@BrianAlienWare>
2014-11-26 16:00:24 -0600

    test

--- act.informative.c
+++ act.informative.c
@@ -793,38 +793,33 @@ ACMD(do_skills) {
     if (is_abbrev(arg, "weapons")) {
         send_to_char(ch, "Weapon Skills:\r\n");
         for (i = 0; i < NUM_SKILLS; i++) {
-           
+            if IS_SET(spell_info[i].skilltype, SKTYPE_WEAPON)
+            for (i = 0; i < SK_ARRAY_MAX; i++) {
+                if IS_SET(spell_info[i].skilltype, SKTYPE_WEAPON)
+                send_to_char(ch, "%s%-20s: %s %2d [%2d] ", QYEL, skill_names[i], QNRM, GET_SKILL_RANK(ch, i),
+                        GET_SKILL_XP(ch, i));
 
-            if ((i != find_skill_num(help, SKTYPE_WEAPON)) <= 0) 
-                continue;
-
-            
-                for (i = 0; i < SK_ARRAY_MAX; i++) {
-                   
-                        send_to_char(ch, "%s%-20s: %s %2d [%2d] ", QYEL, skill_names[i], QNRM, GET_SKILL_RANK(ch, i),
-                                GET_SKILL_XP(ch, i));
-
-                        if (count % 2 == 1)
-                            send_to_char(ch, "\r\n");
-                        count++;
-                    }
-                }
+                if (count % 2 == 1)
+                    send_to_char(ch, "\r\n");
+                count++;
             }
-        
-    } else if (is_abbrev(arg, "survival")) {
-
-    } else if (is_abbrev(arg, "lore")) {
-
-    } else if (is_abbrev(arg, "magic")) {
-
-    } else if (is_abbrev(arg, "armor")) {
-
-    } else if (is_abbrev(arg, "crafting")) {
-
+        }
     }
-    //else {
-    //    send_to_char(ch, "%s SOMETHING WENT WRONG, TELL AN IMM.%s\r\n", KRED, QNRM);
-    //}
+
+} else if (is_abbrev(arg, "survival")) {
+
+} else if (is_abbrev(arg, "lore")) {
+
+} else if (is_abbrev(arg, "magic")) {
+
+} else if (is_abbrev(arg, "armor")) {
+
+} else if (is_abbrev(arg, "crafting")) {
+
+}
+//else {
+//    send_to_char(ch, "%s SOMETHING WENT WRONG, TELL AN IMM.%s\r\n", KRED, QNRM);
+//}
 
 }
 
