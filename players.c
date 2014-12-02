db51fedb132a339e176b2c79823637dfce9e418e
Brian <Brian@BrianAlienWare>
2014-12-01 20:23:32 -0600

    test

--- players.c
+++ players.c
@@ -902,11 +902,11 @@ static void load_affects(FILE *fl, struct char_data *ch) {
 static void load_skills(struct char_data *ch, const char *line, int mode) {//(FILE *fl, struct char_data *ch) {
     int num = 0, num2 = 0, num3 = 0;
     int i;
-    sscanf(line, "%d %d/%d", &num, &num2, &num3);
+    sscanf(line, "%d %d/%d", num, &num2, &num3);
 
     switch (mode) {
         case LOAD_SKILL:
-            GET_SKILL(ch, i) = num;
+//            GET_SKILL(ch, i) = num;
             GET_SKILL_RANK(ch, num) = num2;
             GET_SKILL_XP(ch, num) = num3;
             break;
