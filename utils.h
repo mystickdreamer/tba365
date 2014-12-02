c974ac1dfd4118c2ba4a1ac1c43c7fc049829434
Brian <Brian@BrianAlienWare>
2014-12-01 18:44:11 -0600

    reverse test

--- utils.h
+++ utils.h
@@ -651,7 +651,7 @@ do                                                              \
 #define GET_SKILL_XP(ch, skill)         ((ch)->player.skills[skill][1])
 #define SET_SKILL(ch, skill, val)           (ch)->player.skills[skill][0] = val
 
-#define GET_SKILL(ch)            ((ch)->player.skills)
+#define GET_SKILL(ch, skill)            (ch)->player.skills[skill]
 //#define GET_SKILL_RANKS(ch, i)		((ch)->player_specials->skills[i])
 
 
