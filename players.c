cb8efe754997a50acf85a52db1b792579d26ac5a
Brian <Brian@BrianAlienWare>
2014-12-06 12:52:27 -0600

    skills

--- players.c
+++ players.c
@@ -712,7 +712,7 @@ void save_char(struct char_data * ch) {
     //fprintf(fl, "Skil:\n");
     for (i = 1; i <= SK_ARRAY_MAX; i++) {
         if (GET_SKILL_RANK(ch, i))
-            fprintf(fl, "Skil: %d %d %d/%d\n", i,  GET_SKILL(ch, i), GET_SKILL_RANK(ch, i), GET_SKILL_XP(ch, i));
+            fprintf(fl, "Skil: %d %d/%d\n", i, GET_SKILL_RANK(ch, i), GET_SKILL_XP(ch, i));
     }
     //}
 
@@ -900,16 +900,16 @@ static void load_affects(FILE *fl, struct char_data *ch) {
 }
 
 static void load_skills(FILE *fl, struct char_data *ch) {
-  int num = 0, num2 = 0, num3, num4;
+  int num = 0, num2 = 0, num3 = 0;
   char line[MAX_INPUT_LENGTH + 1];
  
   do {
     get_line(fl, line);
-    sscanf(line, "%d %d %d %d", &num, &num2, &num3, &num4);
+    sscanf(line, "%d %d/%d", &num, &num2, &num3);
     if (num != 0) {
-      GET_SKILL(ch, num) = num2;
-      GET_SKILL_RANK(ch, num) = num3;
-      GET_SKILL_XP(ch, num) = num4;
+      //GET_SKILL(ch, num) = num2;
+      GET_SKILL_RANK(ch, num) = num2;
+      GET_SKILL_XP(ch, num) = num3;
     }
   } while (num != 0);
 }
