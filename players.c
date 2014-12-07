5684a7edde5ab1282ba27af391f3ba9e14baaa19
Brian <Brian@BrianAlienWare>
2014-12-06 20:38:18 -0600

    skill test

--- players.c
+++ players.c
@@ -908,7 +908,9 @@ static void load_skills(FILE *fl, struct char_data *ch) {
     get_line(fl, line);
     sscanf(line, "%d %d %d/%d", &num, &num2, &num3, &num4);
     if (num != 0) {
-      GET_SKILL(ch, num) = num2;
+        for (i = 1; i <= SK_ARRAY_MAX; i++) {
+      GET_SKILL(ch, i) = num2;
+        }
       GET_SKILL_RANK(ch, num) = num3;
       GET_SKILL_XP(ch, num) = num4;
     }
