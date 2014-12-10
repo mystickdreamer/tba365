182bae529053a5172310139922db73cffb2175d3
Brian <Brian@BrianAlienWare>
2014-12-09 22:00:32 -0600

    test

--- players.c
+++ players.c
@@ -712,7 +712,7 @@ void save_char(struct char_data * ch) {
     //fprintf(fl, "Skil:\n");
     for (i = 1; i <= SK_ARRAY_MAX; i++) {
         if (GET_SKILL_RANK(ch, i))
-            fprintf(fl, "Skil: %d %d %d/%d\n", i, GET_SKILL(ch, i), GET_SKILL_RANK(ch, i), GET_SKILL_XP(ch, i));
+            fprintf(fl, "Skil: %d %d/%d\n", i,/* GET_SKILL(ch, i),*/ GET_SKILL_RANK(ch, i), GET_SKILL_XP(ch, i));
     }
     //}
 
@@ -906,9 +906,8 @@ static void load_skills(FILE *fl, struct char_data *ch) {
 
     do {
         get_line(fl, line);
-        sscanf(line, "%d %d %d/%d", &num, &num2, &num3, &num4);
+        sscanf(line, "%d %d/%d", &num,/* &num2,*/ &num3, &num4);
         if (num != 0) {
-            ch->player.skills[num] = num2;
             //      GET_SKILL(ch, num) = num2;
             GET_SKILL_RANK(ch, num) = num3;
             GET_SKILL_XP(ch, num) = num4;
