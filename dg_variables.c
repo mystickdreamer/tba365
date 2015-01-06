b46b0a66e889455524b3271c2f15af5c6f1df54b
Brian <Brian@BrianAlienWare>
2015-01-05 20:08:15 -0600

    test seperating skill rank and xp

--- dg_variables.c
+++ dg_variables.c
@@ -994,7 +994,7 @@ void find_replacement(void *go, struct script_data *sc, trig_data *trig,
                             amount = one_word(subfield, skillname);
                             skip_spaces(&amount);
                             if (amount && *amount && is_number(amount)) {
-                                int skillnum = find_skill_num(skillname, SKTYPE_WEAPON);
+                                int skillnum = find_skill_num(skill, SKTYPE_WEAPON);
                                 if (skillnum > 0) {
                                     int new_value = MAX(0, MIN(1000, atoi(amount)));
                                     SET_SKILL(c, skillnum, new_value);
